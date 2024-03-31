bf3f8a305ea15a93e82c61e984965da1302a5728
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-03-31 17:56:23 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://brad1234:brad1234@cluster0.3qcg7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://brad1234:*@cluster0.3qcg7.mongodb.net/*?retryWrites=true&w=majority&appName=Cluster0';
